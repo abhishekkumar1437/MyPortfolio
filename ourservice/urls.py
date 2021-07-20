e3484dd97d33a6d30c4c2e6573bf99fda97f203e
Abhishek Kumar <61646510+abhishekkumar1437@users.noreply.github.com>
2021-07-20 22:46:31 +0530

    Update urls.py

--- ourservice/urls.py
+++ ourservice/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include,url,serve
+from django.urls import path,include
 from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
      path('', include('portfolio.urls')),
